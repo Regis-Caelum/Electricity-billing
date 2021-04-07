15a2b25e67ed5d77c353581f019064a31d6c87bb
Regis-Caelum <khanmf@rknec.edu>
2021-04-07 12:26:23 +0530

    Added Basic template

--- ElectricityBilling/src/electricitybilling/ElectricityBilling.java
+++ ElectricityBilling/src/electricitybilling/ElectricityBilling.java
@@ -4,11 +4,64 @@
  * and open the template in the editor.
  */
 package electricitybilling;
-
+import java.util.*;  
 /**
  *
  * @author Sam
  */
+
+class consumer{
+    private String id;
+    private int units;
+    private String name;
+    private String contact;
+    protected int connection;
+    private int rate;
+    
+    //constructor
+    consumer(){
+        this.connection = 0;
+        this.contact = new String();
+        this.id = new String();
+        this.name = new String();
+        this.rate = 4;
+        this.units = 0;
+    }
+    
+    //For new connection new connect method
+    void newconnect (){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Please Enter the Name of the consumer");
+        this.name = sc.nextLine();
+        System.out.println("Please Enter the contact of the consumer");
+        this.contact = sc.nextLine();
+        this.connection = 1;
+        this.units = 0;
+        this.id = getID(4);
+    }
+    
+    void updateconnection(){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Please Enter the consumer ID");
+        String str = sc.nextLine();
+    }
+    //method for generating consumer ID
+    String getID(int n)
+    {
+        String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                                    + "0123456789"
+                                    + "abcdefghijklmnopqrstuvxyz";
+        StringBuilder sb = new StringBuilder(n);
+  
+        for (int i = 0; i < n; i++) {
+            int index= (int)(AlphaNumericString.length()* Math.random());
+            sb.append(AlphaNumericString.charAt(index));
+        }
+  
+        return sb.toString();
+    }
+}
+
 public class ElectricityBilling {
 
     /**
